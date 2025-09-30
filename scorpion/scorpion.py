d4a66f94d65431874de20797cc3417e1ae206ce2
Elio Bonutto <elio.bonutto@gmail.com>
2025-09-30 18:15:30 +0200

    feat: creating GUI

--- scorpion/scorpion.py
+++ scorpion/scorpion.py
@@ -2,50 +2,74 @@ import argparse
 from colorama import Fore, Style
 from PIL import Image
 from PIL.ExifTags import TAGS
+import tkinter as tk
 
 
 VALID_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.gif', '.bmp')
 
 
 def extract_metadata(image):
-	image = Image.open(image)
+	try:
+		image = Image.open(image)
+
+	except Exception as e:
+		print(Fore.RED + f'{image}: cannot open file' + Style.RESET_ALL)
+		return
 
 	info_dict = {
-		"Filename": image.filename,
-		"Image Size": image.size,
-		"Image Height": image.height,
-		"Image Width": image.width,
-		"Image Format": image.format,
-		"Image Mode": image.mode,
-		"Image is Animated": getattr(image, "is_animated", False),
-		"Frames in Image": getattr(image, "n_frames", 1)
+		'Filename': image.filename,
+		'Image Size': image.size,
+		'Image Height': image.height,
+		'Image Width': image.width,
+		'Image Format': image.format,
+		'Image Mode': image.mode,
+		'Image is Animated': getattr(image, 'is_animated', False),
+		'Frames in Image': getattr(image, 'n_frames', 1)
 	}
 
-	for label,value in info_dict.items():
-		print(f"{label:25}: {value}")
+	output = ''
+	for k, v in info_dict.items():
+		output += f"{k:15}: {v}\n"
 	
 	exifdata = image.getexif()
-
 	for tag_id in exifdata:
-		# get the tag name, instead of human unreadable tag id
-		tag = TAGS.get(tag_id, tag_id)
-		data = exifdata.get(tag_id)
-		# decode bytes 
+		tag = TAGS.get(tag_id, tag_id) # get the tag name, instead of human unreadable tag id
+		data = exifdata.get(tag_id)    # decode bytes 
 		if isinstance(data, bytes):
-			data = data.decode()
-		print(f"{tag:25}: {data}")
+			try:
+				data = data.decode(errors='ignore')
+			except:
+				data = "<binary>"
+		output += f"{tag:15}: {data}\n"
+	return output
+
+
+def create_interface(args):
+	root = tk.Tk()           # Create window
+	root.title("scorpion")   # Title
+	root.geometry("700x500") # Size
+
+	metadata_text = tk.Text(root, width=80, height=25)
+	metadata_text.pack(padx=10, pady=10)
+
+	for image in args.images:
+		if any(image.endswith(ext) for ext in VALID_EXTENSIONS):
+			metadata_text.insert(tk.END, f"--- {image} ---\n")
+			metadata_text.insert(tk.END, extract_metadata(image))
+			metadata_text.insert(tk.END, "\n\n")
+
+	root.mainloop()
+
+
 
 def scorpion():
 	parser = argparse.ArgumentParser(prog='spider.py', description='Parse image files for metadata')
 	parser.add_argument('images', nargs='+', help='Image files to analyze')
-	
+	parser.add_argument('-m', '--modify', action='store_true', help='Modify or delete metadata of images')
+
 	args = parser.parse_args() #! Attention aux Doublons
 
-	for image in args.images:
-		print(Fore.GREEN + 'File:' + Style.RESET_ALL, image)
-	
-	for image in args.images:
-		extract_metadata(image)
+	create_interface(args)
 
 
 if __name__ == '__main__':
